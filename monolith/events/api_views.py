d839308df6e367b77572e4b05d32c660cc6b425e
Minting <miz111@ucsd.edu>
2022-11-23 16:55:40 -0800

    created location and sent to server

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -229,3 +229,15 @@ def api_show_location(request, pk):
             encoder=LocationDetailEncoder,
             safe=False,
         )
+
+@require_http_methods(["GET"])
+def api_list_states(request):
+    states = State.objects.all().order_by('name')
+    state_list = []
+    for state in states:
+        d = {
+            "name": state.name,
+            "abbreviation": state.abbreviation
+            }
+        state_list.append(d)
+    return JsonResponse({"states": state_list})
